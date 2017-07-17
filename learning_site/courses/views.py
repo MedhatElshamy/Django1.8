8389f8ec9df343116e0214ee83de4d2eb550a52e
Medhat Elshamy <medhat.elshamy7@gmail.com>
2017-07-17 09:30:22 +0300

    First App View

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Course
+from django.http import HttpResponse
 # Create your views here.
+
+def courses_list(request):
+	courses = Course.objects.all()
+	output = ', '.join([str(course) for course in courses])
+	return HttpResponse(output)
--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -16,7 +16,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.hello_world)
+    url(r'^$', views.hello_world),
+    url(r'^courses/', include('courses.urls')),
 ]
