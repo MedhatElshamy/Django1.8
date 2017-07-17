66252e8badc338252065543ef17fafa07a64d7e9
Medhat Elshamy <medhat.elshamy7@gmail.com>
2017-07-17 18:45:31 +0300

    Add a Detail View

--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from . import views
 urlpatterns = [
-	url(r'^$', views.course_list)
+	url(r'^$', views.course_list),
+	url(r'(?P<pk>\d+)/$', views.course_detail)
 ]
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -12,3 +12,7 @@ from django.http import HttpResponse
 def course_list(request):
 	courses = Course.objects.all()
 	return render(request, 'courses/course_list.html', {'courses': courses})
+
+def course_detail(request, pk):
+	course = Course.objects.get(pk = pk)
+	return render(request, 'courses/course_detail.html', {'course': course})
