93a595f446b1f46bcdebbc317d4acd223efcf7f1
Medhat Elshamy <medhat.elshamy7@gmail.com>
2017-07-17 11:25:31 +0300

    Templates

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -3,8 +3,12 @@ from .models import Course
 from django.http import HttpResponse
 # Create your views here.
 
+# def courses_list(request):
+# 	courses = Course.objects.all()
+# 	output = ', '.join([str(course) for course in courses])
+	
+# 	return HttpResponse(output)
+
 def courses_list(request):
 	courses = Course.objects.all()
-	output = ', '.join([str(course) for course in courses])
-	
-	return HttpResponse(output)
+	return render(request, 'courses/course_list.html', {'courses': courses})
